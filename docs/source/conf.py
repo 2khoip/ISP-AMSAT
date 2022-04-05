4f9c6534b7c04626f5c6510ed218b9706928aef0
KOIDABAWS <43629728+KOIDABAWS@users.noreply.github.com>
2022-04-05 00:14:43 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ISP-AMSAT/GOES'
+copyright = '2021, Pham'
+author = 'Pham'
 
 release = '0.1'
 version = '0.1.0'
